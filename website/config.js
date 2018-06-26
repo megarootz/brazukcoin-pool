763d1d4a3eb41c656bb2ba33ea99ea0ac16c3d3e
bzk <bzk@brazukcoin.org>
2018-06-26 18:02:03 -0300

    customizations

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "http://127.0.0.1:8117";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "";
+
+var irc = "";
+
+var email = "";
+
+var cryptonatorWidget = ["BZK-BTC"];
+
+var easyminerDownload = "";
+
+var blockchainExplorer = "http://chainradar.com/xmr/block/";
+
+var transactionExplorer = "http://chainradar.com/xmr/transaction/";
